218aafcb627256f9202b1779ae915f7c25c986c4
sampresh <slevinshrestha@gmail.com>
2020-04-03 17:48:30 +1000

    CRUD operation WIP

--- Core/Database.py
+++ Core/Database.py
@@ -8,6 +8,12 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 myCursor = connection.cursor() #needed to fire query of mysql
+
+# #----------- list the databases
+# myCursor.execute("SHOW DATABASES")
+# for x in myCursor:
+#     print(x)
+
 ## ----------creates the table 'names' inside the database
 # myCursor.execute("""CREATE TABLE names
 #     (
@@ -16,16 +22,39 @@ myCursor = connection.cursor() #needed to fire query of mysql
 #     )
 #     """)
 
+# #--------alter the table
+# myCursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
 ## ------inserts data into the table with given values
 # myCursor.execute("""INSERT INTO names(id,name) VALUES(1,"Sam")
 #
 #     """)
 
+# # -------- update the table with values
 # myCursor.execute("UPDATE name FROM names WHERE id = '1';")
 
+# #--------delete the data from table
 # myCursor.execute("DELETE FROM names where id='1';")
 
+# display the data
 myCursor.execute("SELECT * FROM names;")
-print(myCursor.fetchall(), end="\n")
+myResult = myCursor.fetchall()
+for x in myResult:
+    print(x)
+
 connection.commit()
 connection.close()
+
+# #escaping the values - Escape query values by using the placholder %s method. Eg.sql = "SELECT * FROM customers WHERE address = %s" ;   adr = ("Yellow Garden 2", )
+## Setting the offset from the 3rd item; mycursor.execute("SELECT * FROM customers LIMIT 5 OFFSET 2")
+
+# # Inserting multiple data
+# sql = """INSERT INTO names (id,name) VALUES (%s, %s)
+#     """
+# value = [
+#     (2,'ram'),
+#     (3,"shyam")
+# ]
+# myCursor.executemany(sql,value) #to execute many queries at once
+# connection.commit()
+# connection.close()
--- readme.txt
+++ readme.txt
@@ -2,7 +2,13 @@ Readme notes for Python and NumPy
 ----------------------------------
 This is just a practice pilot project for studying Python and Pycharm
 
-Files included:
+Folders included:
+Core - core studies
+CRUD Python - Database creation and basic crud operation
+NumPy - methods in relation to NumPy application
+--------------------------------
+Files included (Core directory) :
+
 first.py : learning the cores with comments and documentation included
 second.py : learning the cores
 question.py : Question class created to 
